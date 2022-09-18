871031d679f340149cb42d178a36c689da653f7f
koldodan <44589292+koldodan@users.noreply.github.com>
2022-09-18 10:28:11 -0400

    Update streamlit_app.py
    added header, text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
-streamlit.title("My Parents New Healthy Dinner")
+streamlit.title("Snowflake GitHub, Streamlit, and Python")
+streamlit.header("Love coding")
+streamlit.test("In a beautiful Sunday morning!")
+streamlit.test("Enjoy!")
 
