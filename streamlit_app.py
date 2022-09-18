7910c4c954e13fbaf124b32d3b75b14de4f1f241
koldodan <44589292+koldodan@users.noreply.github.com>
2022-09-18 10:28:43 -0400

    Update streamlit_app.py
    spelling

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title("Snowflake GitHub, Streamlit, and Python")
 streamlit.header("Love coding")
-streamlit.test("In a beautiful Sunday morning!")
-streamlit.test("Enjoy!")
+streamlit.text("In a beautiful Sunday morning!")
+streamlit.text("Enjoy!")
 
