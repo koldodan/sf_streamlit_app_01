f2a24023b8d9793b967ac0f023df50e2601e31f3
koldodan <44589292+koldodan@users.noreply.github.com>
2022-09-18 10:36:38 -0400

    Update streamlit_app.py
    make it interactive - add multi-select and display

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,5 +7,10 @@ streamlit.text("In a beautiful Sunday morning!")
 streamlit.text("Enjoy!")
 streamlit.text("Well it is also time for breakfast 🍌🥭  :-)")
 
+# add fruit list from S3
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+# Display the table on the page.
+my_fruit_list = my_fruit_list.set_index('Fruit')
