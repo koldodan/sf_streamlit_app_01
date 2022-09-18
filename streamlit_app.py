5e4cfedefeef8cf4106488c5cdcec3ac89262a79
koldodan <44589292+koldodan@users.noreply.github.com>
2022-09-18 10:30:48 -0400

    Update streamlit_app.py
    add breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
-streamlit.title("Snowflake GitHub, Streamlit, and Python")
+streamlit.title("Snowflake, GitHub, Streamlit, and Python")
 streamlit.header("Love coding")
 streamlit.text("In a beautiful Sunday morning!")
 streamlit.text("Enjoy!")
+streamlit.text("Well it is also time for breakfast 🍌🥭  :-)")
 
