384f18de8caf7956753f3f9ae270f213854965f0
koldodan <44589292+koldodan@users.noreply.github.com>
2022-09-18 10:34:10 -0400

    Update streamlit_app.py
    added fruit list from s3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
+import pandas
+
 streamlit.title("Snowflake, GitHub, Streamlit, and Python")
 streamlit.header("Love coding")
 streamlit.text("In a beautiful Sunday morning!")
 streamlit.text("Enjoy!")
 streamlit.text("Well it is also time for breakfast 🍌🥭  :-)")
 
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
