bbcb1d47b3e7ce8b51b7f463f83ffaeeb6090fe6
koldodan <44589292+koldodan@users.noreply.github.com>
2022-09-19 23:41:39 -0400

    Update streamlit_app.py
    2

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,5 @@ streamlit.header("fruityvice in tabular form - pandas dataframe")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
